4ec767fb961e09d6b8e5026befc2aa5c39120a2a
opilane <opilane@HP-CZC2349HT2>
2026-09-18 11:23:17 +0300

    vali raskus

--- MängForm.cs
+++ MängForm.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 using Timer = System.Windows.Forms.Timer;
 
@@ -8,13 +7,15 @@ namespace programmeerimine2
 {
     public partial class MängForm : Form
     {
-        private Label aegSilt = new Label { Text = "aeg: 30", Location = new Point(200, 10), AutoSize = true };
-        private Label l1 = new Label { Location = new Point(20, 40), AutoSize = true }, l2 = new Label { Location = new Point(20, 70), AutoSize = true };
-        private Label l3 = new Label { Location = new Point(20, 100), AutoSize = true }, l4 = new Label { Location = new Point(20, 130), AutoSize = true };
-        private NumericUpDown n1 = new NumericUpDown { Location = new Point(180, 38), Width = 80, Enabled = false }, n2 = new NumericUpDown { Location = new Point(180, 68), Width = 80, Enabled = false };
-        private NumericUpDown n3 = new NumericUpDown { Location = new Point(180, 98), Width = 80, Enabled = false }, n4 = new NumericUpDown { Location = new Point(180, 128), Width = 80, Enabled = false };
-        private Button alusta = new Button { Text = "alusta", Location = new Point(30, 170), Size = new Size(110, 30) };
-        private Button loobu = new Button { Text = "loobu", Location = new Point(150, 170), Size = new Size(110, 30), Enabled = false };
+        private ComboBox raskus = new ComboBox { Location = new Point(20, 10), Width = 90, DropDownStyle = ComboBoxStyle.DropDownList };
+        private Label aegSilt = new Label { Text = "aeg: 30", Location = new Point(180, 13), AutoSize = true };
+        private Label l1 = new Label { Location = new Point(20, 45), AutoSize = true }, l2 = new Label { Location = new Point(20, 75), AutoSize = true };
+        private Label l3 = new Label { Location = new Point(20, 105), AutoSize = true }, l4 = new Label { Location = new Point(20, 135), AutoSize = true };
+        private NumericUpDown n1 = new NumericUpDown { Location = new Point(180, 43), Width = 80, Maximum = 1000, Enabled = false };
+        private NumericUpDown n2 = new NumericUpDown { Location = new Point(180, 73), Width = 80, Maximum = 1000, Enabled = false };
+        private NumericUpDown n3 = new NumericUpDown { Location = new Point(180, 103), Width = 80, Maximum = 1000, Enabled = false };
+        private NumericUpDown n4 = new NumericUpDown { Location = new Point(180, 133), Width = 80, Maximum = 1000, Enabled = false };
+        private Button alusta = new Button { Text = "alusta", Location = new Point(80, 175), Size = new Size(120, 30) };
         private Timer taimer = new Timer { Interval = 1000 };
 
         private int aeg, v1, v2, v3, v4;
@@ -23,56 +24,49 @@ namespace programmeerimine2
         public MängForm()
         {
             Text = "matemaatika";
-            Size = new Size(300, 250);
+            Size = new Size(300, 260);
+
+            raskus.Items.AddRange(new object[] { "lihtne", "raske" });
+            raskus.SelectedIndex = 0;
 
             alusta.Click += (s, e) => AlustaMängu();
-            loobu.Click += (s, e) => LoobuMängust();
             taimer.Tick += TaimerTiksub;
 
-            Controls.AddRange(new Control[] { aegSilt, l1, n1, l2, n2, l3, n3, l4, n4, alusta, loobu });
+            Controls.AddRange(new Control[] { raskus, aegSilt, l1, n1, l2, n2, l3, n3, l4, n4, alusta });
         }
 
         private void AlustaMängu()
         {
-            int a = rand.Next(1, 20), b = rand.Next(1, 20);
+            bool onRaske = raskus.SelectedIndex == 1;
+            int max = onRaske ? 50 : 20;
+            int maxM = onRaske ? 15 : 10;
+
+            int a = rand.Next(1, max), b = rand.Next(1, max);
             v1 = a + b; l1.Text = $"{a} + {b} ="; n1.Value = 0; n1.Enabled = true;
 
-            a = rand.Next(10, 20); b = rand.Next(1, a);
+            a = rand.Next(max / 2, max); b = rand.Next(1, a);
             v2 = a - b; l2.Text = $"{a} - {b} ="; n2.Value = 0; n2.Enabled = true;
 
-            a = rand.Next(1, 10); b = rand.Next(1, 10);
+            a = rand.Next(2, maxM); b = rand.Next(2, maxM);
             v3 = a * b; l3.Text = $"{a} * {b} ="; n3.Value = 0; n3.Enabled = true;
 
-            b = rand.Next(1, 10); a = b * rand.Next(1, 10);
+            b = rand.Next(2, maxM); a = b * rand.Next(2, maxM);
             v4 = a / b; l4.Text = $"{a} / {b} ="; n4.Value = 0; n4.Enabled = true;
 
-            aeg = 30;
+            aeg = onRaske ? 20 : 30;
             aegSilt.Text = $"aeg: {aeg}";
             alusta.Enabled = false;
-            loobu.Enabled = true;
+            raskus.Enabled = false;
             taimer.Start();
-        }
-
-        private void LoobuMängust()
-        {
-            n1.Value = v1; n2.Value = v2; n3.Value = v3; n4.Value = v4;
-            LõpetaMäng("andsid alla!");
-        }
-
-        private void LõpetaMäng(string teade)
-        {
-            taimer.Stop();
-            n1.Enabled = n2.Enabled = n3.Enabled = n4.Enabled = false;
-            alusta.Enabled = true;
-            loobu.Enabled = false;
-            MessageBox.Show(teade);
         }
 
         private void TaimerTiksub(object sender, EventArgs e)
         {
             if (n1.Value == v1 && n2.Value == v2 && n3.Value == v3 && n4.Value == v4)
             {
-                LõpetaMäng("võit!");
+                taimer.Stop();
+                MessageBox.Show("võit!");
+                LõpetaMäng();
             }
             else if (aeg > 0)
             {
@@ -81,8 +75,20 @@ namespace programmeerimine2
             }
             else
             {
-                LõpetaMäng("aeg läbi!");
+                taimer.Stop();
+                MessageBox.Show("aeg läbi!");
+                LõpetaMäng();
             }
+        }
+
+        private void LõpetaMäng()
+        {
+            alusta.Enabled = true;
+            raskus.Enabled = true;
+            n1.Enabled = false;
+            n2.Enabled = false;
+            n3.Enabled = false;
+            n4.Enabled = false;
         }
     }
 }
